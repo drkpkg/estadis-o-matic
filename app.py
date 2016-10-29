addf7855207cc830374cc7167dc3a6205335ebda
drkpkg <daniel.uremix@gmail.com>
2016-10-29 15:05:02 -0400

    Gitignore con pycache

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, request, jsonify
 from flask import render_template
-
+from libs.est_tendencia_central import TendenciaCentralNoAgrupada
 from libs.frecuencia import Frecuencia
 
 app = Flask(__name__)
@@ -11,22 +11,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello World! This is powered by Python backend."
+    return "Hello World! This is powered by Python backend."
 
 
 @app.route("/tendencia-central")
 def tendencia_central():
-	return render_template('tendencia.html')
+    return render_template('tendencia.html')
 
 
 @app.route("/tendencia-central/agrupada")
 def agrupada():
-	return render_template('agrupada.html')
+    return render_template('agrupada.html')
 
 
 @app.route("/tendencia-central/no-agrupada")
 def no_agrupada():
-	return render_template('nagrupada.html')
+    return render_template('nagrupada.html')
 
 
 """Post routes"""
@@ -34,17 +34,24 @@ def no_agrupada():
 
 @app.route("/tendencia-central/calcular_tendencia_agrupada", methods=['POST'])
 def calcular_tendencia_agrupada():
-	return jsonify(status=200)
+    return jsonify(status=200)
 
 
 @app.route("/tendencia-central/calcular_tendencia_no_agrupada", methods=['POST'])
 def calcular_tendencia_no_agrupada():
-	data = list(map(int, request.form['data'].split(',')))
-	data.sort()
-	frecuencia = Frecuencia(data)
-	frecuencia.crear_tabla()
-	return jsonify(status=200, f=frecuencia.tabla)
+    data = list(map(int, request.form['data'].split(',')))
+    data.sort()
+    frecuencia = Frecuencia(data)
+    frecuencia.crear_tabla()
+    fna = TendenciaCentralNoAgrupada(data)
+    return jsonify(status=200,
+                   tabla=frecuencia.tabla,
+                   tc={'media': fna.media(),
+                       'mediana': fna.mediana(),
+                       'armonica': fna.media_geometrica(),
+                       'geometrica': fna.media_armonica(),
+                       'moda': fna.moda()})
 
 
 if __name__ == "__main__":
-	app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=3000)
--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -5,25 +5,40 @@ $(function(){
 
 function loadEvents(){
 	$('#tabled').hide();
+	$('#tc').hide();
+
 	$('#submit-nag').click(function(){
 		var data = $('#data-numbers').val()
+		$('#submit-nag').hide();
+
 		$.ajax({
             url: '/tendencia-central/calcular_tendencia_no_agrupada',
             data: {data: data},
+            sync: true,
             type: 'POST',
             success: function(response) {
+                $('#tabledbody').empty();
+                $('#tc').empty();
 
-                var k = 0;
-                $.each(response, function(i,v){
-					console.log([i,v]);
-//					$('#tabledbody').empty().append("<tr><td>"+ m['intervalo'] +"</td><td>"+ m['marca de clase'] +"</td><td>"+ m['frecuencia relativa'] +"</td><td>"+ m['frecuencia relativa acumulada'] +"</td></tr>");
-					k += 1;
-                });
+                $.each(response.tabla,function(i,data){
+			        $('#tabledbody').append("<tr><td>" + data['intervalo'][0] + "-" + data['intervalo'][1] + "</td><td>" + data['marca de clase']
+					                        + "</td><td>" + data['frecuencia relativa'] + "</td><td>" + data['frecuencia relativa acummulada']
+					                        +"</td></tr>");
+				});
+
+                $('#tc').append("<div class='form-group'><label>Media: " + response.tc.media + "</label></div>");
+                $('#tc').append("<div class='form-group'><label>Mediana: " + response.tc.mediana + "</label></div>");
+                $('#tc').append("<div class='form-group'><label>Moda: " + response.tc.moda + "</label></div>");
+                $('#tc').append("<div class='form-group'><label>Media Armónica: " + response.tc.armonica + "</label></div>");
+                $('#tc').append("<div class='form-group'><label>Media Geométrica: " + response.tc.geometrica + "</label></div>");
+
                 $('#tabled').show();
+                $('#tc').show();
             },
             error: function(error) {
                 console.log(error);
             }
         });
+        $('#submit-nag').show();
 	});
 }
